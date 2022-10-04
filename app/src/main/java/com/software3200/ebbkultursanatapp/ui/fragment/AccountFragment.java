fd00ac8e39a4b38490f0e5e54fa40d1058873fa6
FatihPc <fkilic1805@gmail.com>
2022-10-04 12:39:22 +0300

    Initial commit

---
+++ app/src/main/java/com/software3200/ebbkultursanatapp/ui/fragment/AccountFragment.java
@@ -0,0 +1,36 @@
+package com.software3200.ebbkultursanatapp.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.software3200.ebbkultursanatapp.databinding.FragmentAccountBinding;
+
+
+public class AccountFragment extends Fragment {
+
+    private FragmentAccountBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
