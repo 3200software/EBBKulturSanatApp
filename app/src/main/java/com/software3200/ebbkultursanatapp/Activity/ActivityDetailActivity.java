9eabe440a28a702e35e01dee623ce77ed1cc765c
FatihPc <fkilic1805@gmail.com>
2022-10-18 17:27:37 +0300

    18102022 - 1727 - PC

--- app/src/main/java/com/software3200/ebbkultursanatapp/Activity/ActivityDetailActivity.java
+++ app/src/main/java/com/software3200/ebbkultursanatapp/Activity/ActivityDetailActivity.java
@@ -1,12 +1,23 @@
 package com.software3200.ebbkultursanatapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.software3200.ebbkultursanatapp.R;
 
 public class ActivityDetailActivity extends AppCompatActivity {
+
+
+    FirebaseFirestore firebaseFirestore;
+
+    String activityDetailDocumntID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,39 @@ public class ActivityDetailActivity extends AppCompatActivity {
 
 
 
+
+
     }
+
+
+    public void getActivityDetail() {
+
+
+
+        firebaseFirestore.collection("Activity").document(activityDetailDocumntID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if (task.isSuccessful()) {
+
+                    DocumentSnapshot documemnt = task.getResult();
+                    if (documemnt.exists()) {
+
+                        String activitytitle = (String) documemnt.get("activityTitle");
+
+
+                    }
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+    }
+
 }
