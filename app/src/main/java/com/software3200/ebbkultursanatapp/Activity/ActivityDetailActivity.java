6113fc8ac0851ba7496be12f6da978c7d3925987
FatihKilic1905 <fkilic1805@gmail.com>
2022-10-26 09:00:19 +0300

    261022 - 0859 - Laptop

--- app/src/main/java/com/software3200/ebbkultursanatapp/Activity/ActivityDetailActivity.java
+++ app/src/main/java/com/software3200/ebbkultursanatapp/Activity/ActivityDetailActivity.java
@@ -4,18 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.software3200.ebbkultursanatapp.R;
+import com.software3200.ebbkultursanatapp.databinding.ActivityDetailBinding;
+import com.software3200.ebbkultursanatapp.databinding.ActivityMainBinding;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ActivityDetailActivity extends AppCompatActivity {
 
+    ActivityDetailBinding binding;
 
     FirebaseFirestore firebaseFirestore;
 
@@ -28,6 +34,8 @@ public class ActivityDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
 
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
 
@@ -64,6 +72,72 @@ public class ActivityDetailActivity extends AppCompatActivity {
                         Long activityLocationLatitude = (Long) documemnt.get("activityLocationLatitude");
                         Long activityLocationLongitude = (Long) documemnt.get("activityLocationLongitude");
                         String activityTelephoneNumber = (String) documemnt.get("activiactivityTelephoneNumbertyTitle");
+
+
+
+                        Picasso.get().load(activityImgUrl).into(binding.activityImageView);
+                        binding.activityTitleTextView.setText(activitytitle);
+                        binding.activityLocationTextView.setText(activityLocation);
+                        binding.activityCategoryTextView.setText(activityCategory);
+
+
+                        if (activityEndDate == null) {
+
+                            Calendar cal = Calendar.getInstance();
+                            cal.setTime(activityDate);
+
+
+                            int dayInt = cal.get(Calendar.DAY_OF_MONTH);
+                            String month = new SimpleDateFormat("MMM").format(cal.getTime());
+                            String day = new SimpleDateFormat("EEEE").format(cal.getTime());
+                            String time = new SimpleDateFormat("HH:mm").format(cal.getTime());
+
+                            binding.activitDateDayNumberTextView.setText(dayInt);
+                            binding.activityDateMonthTextView.setText(month);
+                            binding.activiyDateDayTextview.setText(day);
+
+
+                            binding.activityDateBrace.setVisibility(View.INVISIBLE);
+                            binding.activityDateBrace.getLayoutParams().width = 0;
+                            binding.activitDateDayNumberTextView2.setVisibility(View.INVISIBLE);
+                            binding.activitDateDayNumberTextView2.setWidth(0);
+                            binding.activityDateMonthTextView2.setVisibility(View.INVISIBLE);
+                            binding.activityDateMonthTextView2.setWidth(0);
+                            binding.activiyDateDayTextview2.setVisibility(View.INVISIBLE);
+                            binding.activiyDateDayTextview2.setWidth(0);
+
+
+                        } else {
+
+
+                            Calendar beginCal = Calendar.getInstance();
+                            beginCal.setTime(activityBeginDate);
+
+                            int dayInt = beginCal.get(Calendar.DAY_OF_MONTH);
+                            String month = new SimpleDateFormat("MMM").format(beginCal.getTime());
+                            String day = new SimpleDateFormat("EEEE").format(beginCal.getTime());
+                            String time = new SimpleDateFormat("HH:mm").format(beginCal.getTime());
+
+                            binding.activitDateDayNumberTextView.setText(dayInt);
+                            binding.activityDateMonthTextView.setText(month);
+                            binding.activiyDateDayTextview.setText(day);
+
+                            Calendar endCal = Calendar.getInstance();
+                            endCal.setTime(activityEndDate);
+
+                            int dayIntend = endCal.get(Calendar.DAY_OF_MONTH);
+                            String monthend = new SimpleDateFormat("MMM").format(endCal.getTime());
+                            String dayend = new SimpleDateFormat("EEEE").format(endCal.getTime());
+
+
+                            binding.activitDateDayNumberTextView2.setText(dayIntend);
+                            binding.activityDateMonthTextView2.setText(monthend);
+                            binding.activiyDateDayTextview2.setText(dayend);
+
+
+
+
+                        }
 
 
 
